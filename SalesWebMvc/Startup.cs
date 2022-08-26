518fa66be4116b45b06fa668465fe4fc13518c8b
Diego Nobre <diegonobre.dev@outlook.com>
2022-08-26 14:47:34 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -16,7 +16,7 @@ namespace SalesWebMvc
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            Configuration = configuration; 
         }
 
         public IConfiguration Configuration { get; }
