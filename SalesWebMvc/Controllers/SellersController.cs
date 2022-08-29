9bfc4182422b8827a1fb808577e10d04a2897a1e
Diego Nobre <diegonobre.dev@outlook.com>
2022-08-29 17:58:10 -0300

    Foreing key not null (referential integrity)

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -10,23 +10,34 @@ namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
+        
+        /// <summary>
+        ///criação do controller de vendendores manualmente 
+        /// </summary>
+
+        //depedencia do banco de dados 
         private readonly SellerService _sellerService;
 
+        //criação do construtor 
         public SellersController(SellerService sellerService)
         {
             _sellerService = sellerService;
         }
+
+        //ação para a visualição em lista dos vendendores..
         public IActionResult Index()
         {
             var list = _sellerService.FindAll();
             return View(list);
         }
-
+        //ação para adicionar um novo vendendor.
         public IActionResult Create()
         {
             return View();
         }
         
+
+        //ação para adicionar o novo vendendor no banco de dados
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -8,20 +8,29 @@ namespace SalesWebMvc.Services
 {
     public class SellerService
     {
+        /// <summary>
+        /// classe auxiliar do controlador Seller.
+        /// </summary>
+        
+        //depedencia do banco de dados.
         private readonly SalesWebMvcContext _context;
 
+        //criação de construtor.
         public SellerService(SalesWebMvcContext context)
         {
             _context = context;
         }
 
+       //metodo para retornar as informações da Class "Seller" em forma de lista.
         public List<Seller> FindAll()
         {
             return _context.Seller.ToList();
         }
-
+        //metodo para adicionar o "obj" no banco de dados.
         public void Insert(Seller obj)
         {
+            //associar o primeiro departamento no vendendor
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
